7cf0ceef84bdaee90b58c8f4c0557104f606eb5d
John McDonald <mcdona22@gmail.com>
2026-08-06 18:04:26 +0100

    method to process the image file created

--- lib/features/bill_analyse/service/gemini_service.dart
+++ lib/features/bill_analyse/service/gemini_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/services.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:http/http.dart' as http;
 import 'package:loggy/loggy.dart';
@@ -42,21 +43,29 @@ class GeminiService with UiLoggy {
     return dto;
   }
 
-  String _buildFastReceiptPrompt(String rawText) {
-    return '''
-Extract structured receipt data from raw text into JSON.
+  Future<ReceiptDto> analyzeAssetReceipt(String path) async {
+    final ByteData byteData = await rootBundle.load(path);
+    final Uint8List bytes = byteData.buffer.asUint8List();
+    final String mimeType = 'image/jpeg';
 
-SPEED & EXECUTION DIRECTIVE:
-- Do NOT perform internal reasoning, planning, or step-by-step thinking.
-- Output the raw JSON schema directly in a single pass.
-- If a value (like item name, price, or quantity) is unreadable, obscured, or missing, output null for that field.
-EXTRACTION DIRECTIVES:
-- Do NOT list service charges, gratuities, or discretionary tips as items in the 'items' array.
-- Extract any explicit service charge or gratuity into 'serviceCharge'.
-- Extract the subtotal before service charge into 'subtotal' if available.
-RAW TEXT:
-$rawText
-''';
+    final response = await model.generateContent([
+      Content.multi([TextPart(prompt), DataPart(mimeType, bytes)]),
+    ]);
+
+    if (response.text == null || response.text!.isEmpty) {
+      loggy.error('Gemini error occurred processing image file');
+      throw Exception('Gemini Exception');
+    }
+
+    final json = jsonDecode(response.text!) as Map<String, dynamic>;
+    final dto = ReceiptDto.fromJson(json);
+    loggy.debug("Response found");
+    loggy.debug('DTO: \n$dto');
+    return dto;
+  }
+
+  String _buildFastReceiptPrompt(String rawText) {
+    return ' $prompt\nRAWTEXT: $rawText \nRAW TEXT:$rawText';
   }
 
   Future<List<String>> listAvailableModels(String apiKey) async {
@@ -103,3 +112,15 @@ $rawText
     }).toList();
   }
 }
+
+const prompt = '''Extract structured receipt data from raw text into JSON.
+
+SPEED & EXECUTION DIRECTIVE:
+- Do NOT perform internal reasoning, planning, or step-by-step thinking.
+- Output the raw JSON schema directly in a single pass.
+- If a value (like item name, price, or quantity) is unreadable, obscured, or missing, output null for that field.
+EXTRACTION DIRECTIVES:
+- Do NOT list service charges, gratuities, or discretionary tips as items in the 'items' array.
+- Extract any explicit service charge or gratuity into 'serviceCharge'.
+- Extract the subtotal before service charge into 'subtotal' if available.
+''';
