e2d98cb8b32316515df352df9a92cd7787110a24
John McDonald <mcdona22@gmail.com>
2026-08-15 10:35:39 +0100

    flutterfire configure generated files

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_loggy/flutter_loggy.dart';
@@ -8,6 +9,7 @@ import 'package:tab_settle/core/theme/themes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await dotenv.load(fileName: ".env");
 
