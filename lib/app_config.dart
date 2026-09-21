1496c0e29b56ab686d17d21cb52aee19ae3c3605
John McDonald <mcdona22@gmail.com>
2026-09-21 14:53:19 +0100

    app config update

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,7 +1,6 @@
 abstract class AppConfig {
   static const String appTitle = 'Tab Share';
-  static const String version = '1.0.1 - capture overhaul'; // Version + Build
-  // Number
+  static const String version = '1.0.2 - explicit service messages';
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
     defaultValue:
